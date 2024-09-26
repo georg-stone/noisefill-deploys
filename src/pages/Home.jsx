1bb61f258907f42cfed57f523a27cab29dab6a50
georg-stone <georgestone@vortx.me>
2024-09-25 20:40:55 -0400

    Add sleep timer and Underwater soundscape

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -10,7 +10,20 @@ import {
   AlertDialogTitle,
   AlertDialogTrigger,
 } from "../components/ui/alert";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "../components/ui/dialog";
+import { Label } from "../components/ui/label";
+import { Input } from "../components/ui/input";
 import { Analytics } from "@vercel/analytics/react";
+
+let timer;
 
 let soundscapes = [
   {
@@ -18,8 +31,9 @@ let soundscapes = [
     emoji: "🌊",
     url: "https://utfs.io/f/VU8He2t54NdYu8EVsK5tgWb3e9PanFUMzSxQm0HhV1XofujB",
     volume: 1,
-    attribution:
+    attribution: [
       "Seawash (calm)  by craiggroshek -- https://freesound.org/s/176617/ -- License: Creative Commons 0",
+    ],
     image: "https://utfs.io/f/VU8He2t54NdYnavqSh6Uydx5HzbJtTENYqUwVaPOXZCnAiK2",
     index: 0,
   },
@@ -28,8 +42,9 @@ let soundscapes = [
     emoji: "🌴",
     url: "https://utfs.io/f/VU8He2t54NdYuNACgha5tgWb3e9PanFUMzSxQm0HhV1Xofuj",
     volume: 1,
-    attribution:
+    attribution: [
       "Birds In Spring (Scotland) by BurghRecords -- https://freesound.org/s/463903/ -- License: Creative Commons 0",
+    ],
     image: "https://utfs.io/f/VU8He2t54NdYpgBC9am76CiVAS4EwQty3arMPfHR1bxgdkZD",
     index: 1,
   },
@@ -38,8 +53,9 @@ let soundscapes = [
     emoji: "💦",
     url: "https://utfs.io/f/VU8He2t54NdY9vI0WdS2OVPpzlUIsm50S3eRo4JLb68vxBYA",
     volume: 1,
-    attribution:
+    attribution: [
       "Rain.wav by idomusics -- https://freesound.org/s/518863/ -- License: Creative Commons 0",
+    ],
     image: "https://utfs.io/f/VU8He2t54NdYObTBgr45tUV7W1K4ESdzvZfN8Pr2yCwGuTiB",
     index: 2,
   },
@@ -48,8 +64,9 @@ let soundscapes = [
     emoji: "🪨",
     url: "https://utfs.io/f/VU8He2t54NdYd9CJeYhMOCr41owzn9sPYh5cNKJQFBEtaWu0",
     volume: 0.8,
-    attribution:
+    attribution: [
       "river small brook stream with rolling splashy good detail.flac by kyles -- https://freesound.org/s/454155/ -- License: Creative Commons 0",
+    ],
     image: "https://utfs.io/f/VU8He2t54NdYK6sDVKYu2OlbUPXGzdjtJ5iT6AaRH0yZuqD8",
     index: 3,
   },
@@ -58,8 +75,9 @@ let soundscapes = [
     emoji: "💨",
     url: "https://utfs.io/f/VU8He2t54NdYhES01SIQ6Taob8Wf0SXDOuUA1VKkE9IHx4qd",
     volume: 1,
-    attribution:
+    attribution: [
       "wind.ogg by sleepCircle -- https://freesound.org/s/22331/ -- License: Creative Commons 0",
+    ],
     image: "https://utfs.io/f/VU8He2t54NdYvQshHTaHAWjPnCZrtxmV56SkaM3oO0qw4huf",
     index: 4,
   },
@@ -68,8 +86,9 @@ let soundscapes = [
     emoji: "🔥",
     url: "https://utfs.io/f/VU8He2t54NdYGNe8h39BnItq9LXQlVPu4jNzU1xdaYCM0pF8",
     volume: 1,
-    attribution:
+    attribution: [
       "Bonfire by forfie -- https://freesound.org/s/364992/ -- License: Creative Commons 0",
+    ],
     image: "https://utfs.io/f/VU8He2t54NdYpRQh5Mcm76CiVAS4EwQty3arMPfHR1bxgdkZ",
     index: 5,
   },
@@ -78,8 +97,9 @@ let soundscapes = [
     emoji: "🌵",
     url: "https://utfs.io/f/VU8He2t54NdYHpvbBvYhmu5O2LJfYdtvzgw0s3nbQXlkZDFS",
     volume: 1,
-    attribution:
+    attribution: [
       "Desert Simple.wav by Proxima4 -- https://freesound.org/s/104320/ -- License: Creative Commons 0",
+    ],
     image: "https://utfs.io/f/VU8He2t54NdYOYYMxdZ45tUV7W1K4ESdzvZfN8Pr2yCwGuTi",
     index: 6,
   },
@@ -89,8 +109,9 @@ let soundscapes = [
     url: "https://utfs.io/f/VU8He2t54NdY6fCCfMVNjR9Nmtg7h50VGWKc8AQoryMUblvI",
     volume: 0.6,
     image: "https://utfs.io/f/VU8He2t54NdYxIBXaQ0DONIyCht8a6ZwdKgqEQSTLR51sMYB",
-    attribution:
+    attribution: [
       "Wind__Artic__Cold.wav by cobratronik -- https://freesound.org/s/117136/ -- License: Creative Commons 0",
+    ],
     index: 7,
   },
   {
@@ -99,8 +120,9 @@ let soundscapes = [
     url: "https://utfs.io/f/VU8He2t54NdY59NfzQ6fcCLQl6pk53zFgINtnv9PqHDjbRJy",
     volume: 1,
     image: "https://utfs.io/f/VU8He2t54NdYH7NV0ddYhmu5O2LJfYdtvzgw0s3nbQXlkZDF",
-    attribution:
+    attribution: [
       "water boil.wav by fryzu82 -- https://freesound.org/s/142333/ -- License: Creative Commons 0",
+    ],
     index: 8,
   },
   {
@@ -109,11 +131,134 @@ let soundscapes = [
     url: "https://utfs.io/f/VU8He2t54NdYOGnYUk45tUV7W1K4ESdzvZfN8Pr2yCwGuTiB",
     volume: 0.2,
     image: "https://utfs.io/f/VU8He2t54NdYDAOUVo88fqOGlaboRgjxshLUcB5MT4ZS2iE1",
-    attribution:
+    attribution: [
       "crickets by FreethinkerAnon -- https://freesound.org/s/129678/ -- License: Creative Commons 0",
+    ],
     index: 9,
   },
+  {
+    name: "Underwater",
+    emoji: "🐠",
+    url: "https://utfs.io/f/VU8He2t54NdYrTIK1A7PtLG5Y82xDew0Ncpqo6IhCjBQRZOn",
+    volume: 0.6,
+    image: "https://utfs.io/f/VU8He2t54NdYI934tMkGS15s7ymktfMgw0zeF4dO2HlKZXbu",
+    attribution: [
+      "Underwater Ambience by Fission9 -- https://freesound.org/s/504641/ -- License: Creative Commons 0",
+    ],
+  },
 ];
+
+function SleepTimer() {
+  const [duration, setDuration] = useState(0);
+  const [remainingTime, setRemainingTime] = useState(0);
+  const [isTimerActive, setIsTimerActive] = useState(false);
+
+  useEffect(() => {
+    let timer;
+
+    if (isTimerActive && remainingTime > 0) {
+      timer = setInterval(() => {
+        setRemainingTime((prev) => {
+          if (prev <= 1) {
+            fadeOutAudio();
+            setIsTimerActive(false);
+            return 0;
+          }
+          return prev - 1;
+        });
+      }, 1000);
+    }
+
+    return () => {
+      clearInterval(timer);
+    };
+  }, [isTimerActive, remainingTime]);
+
+  const fadeOutAudio = () => {
+    const audio = document.getElementById("player");
+    const fadeDuration = 1000;
+    const fadeStep = audio.volume / (fadeDuration / 100);
+    let currentVolume = audio.volume;
+
+    const fadeOutInterval = setInterval(() => {
+      currentVolume -= fadeStep;
+      if (currentVolume <= 0) {
+        clearInterval(fadeOutInterval);
+        audio.pause();
+        audio.volume = 0;
+      } else {
+        audio.volume = currentVolume;
+      }
+    }, 100);
+  };
+
+  const handleStartTimer = () => {
+    setRemainingTime(duration);
+    setIsTimerActive(true);
+  };
+
+  const formatTime = (seconds) => {
+    const minutes = Math.floor(seconds / 60);
+    const secs = seconds % 60;
+    return `${String(minutes).padStart(2, "0")}:${String(secs).padStart(
+      2,
+      "0"
+    )}`;
+  };
+
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        <Button variant="outline">⏳ Sleep timer</Button>
+      </DialogTrigger>
+      <DialogContent className="sm:max-w-[425px]">
+        <DialogHeader>
+          <DialogTitle>Sleep Timer</DialogTitle>
+          <DialogDescription>
+            Automatically turn off the sound after a specific amount of time.
+          </DialogDescription>
+        </DialogHeader>
+        <div className="grid gap-4 py-4">
+          <div className="grid grid-cols-4 items-center gap-4">
+            <Label htmlFor="duration" className="text-right">
+              Duration (min)
+            </Label>
+            <Input
+              id="duration"
+              type="number"
+              value={duration / 60}
+              onChange={(e) => setDuration(e.target.value * 60)}
+              className="col-span-3"
+            />
+          </div>
+          <div className="grid grid-cols-4 items-center gap-4">
+            <Label className="text-right">Remaining Time</Label>
+            <span className="col-span-3 text-center">
+              {formatTime(remainingTime)}
+            </span>
+          </div>
+        </div>
+        <DialogFooter>
+          <Button
+            type="button"
+            onClick={handleStartTimer}
+            className={isTimerActive ? "hidden" : ""}
+          >
+            Start timer
+          </Button>
+          <Button
+            type="button"
+            variant="outline"
+            onClick={() => setIsTimerActive(false)}
+            className={isTimerActive ? "" : "hidden"}
+          >
+            Stop timer
+          </Button>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+  );
+}
 
 function CreditsMenu() {
   return (
@@ -131,7 +276,12 @@ function CreditsMenu() {
                   {sound.emoji} {sound.name}
                 </b>
                 <br />
-                {sound.attribution}
+                {sound.attribution.map((attribution, index) => (
+                  <div key={index}>
+                    {attribution}
+                    <br />
+                  </div>
+                ))}
               </div>
             ))}
           </AlertDialogDescription>
@@ -265,6 +415,7 @@ function Home() {
           {playing ? (sound.url == currentURL ? message : "") : ""}
         </Button>
       ))}
+      <SleepTimer />
       <CreditsMenu />
       <Analytics />
     </div>
